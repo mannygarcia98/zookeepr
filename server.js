4b6f2c251061ee42d26b1e93d055d904e4de43b7
mannygarcia98 <mannysg88@gmail.com>
2022-04-10 15:12:12 -0500

    Add heroku

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const { animals } = require("./data/animals.json");
 const express = require("express");
 const { readFileSync } = require("fs");
-// const PORT = process.env.PORT || 3001;
-const PORT = 19812;
-
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
